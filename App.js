1f5b3f7156e6a1a4734f145a78f82b00ace139b0
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2022-04-13 16:03:21 +0900

    redux
    use redux for global state management

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 import AddTodo from "./screens/AddTodo";
 import Home from "./screens/Home";
 
@@ -7,15 +9,17 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen
-          name="Add"
-          component={AddTodo}
-          options={{ presentation: "modal" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen
+            name="Add"
+            component={AddTodo}
+            options={{ presentation: "modal" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
---
+++ redux/todosSlice.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [],
+};
+
+export const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    setTodosReducer: (state, action) => {
+      state.todos = action.payload;
+      console.log(state.todos);
+    },
+    addTodosReducer: (state, action) => {
+      state.todos.push(action.payload);
+    },
+    hideCompletedReducer: (state) => {
+      state.todos = state.todos.filter((todo) => !todo.completed);
+    },
+    updateTodoReducer: (state, action) => {
+      state.todos = state.todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          todo.isCompleted = !todo.isCompleted;
+        }
+        return todo;
+      });
+    },
+    deleteTodoReducer: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const {
+  setTodosReducer,
+  addTodosReducer,
+  updateTodoReducer,
+  hideCompletedReducer,
+  deleteTodoReducer,
+} = todosSlice.actions;
+
+export default todosSlice.reducer;
