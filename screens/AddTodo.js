d8718452d4c7c6dae56beefc31435916fe1d08fd
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2022-04-13 15:24:42 +0900

    Implement react navigation & add task modal

--- screens/AddTodo.js
+++ screens/AddTodo.js
@@ -1,12 +1,92 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Switch,
+  TouchableOpacity,
+} from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function AddTodo() {
+  const [name, setName] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [isToday, setIsToday] = useState(false);
   return (
-    <View>
-      <Text>This is the add todo</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Add Task</Text>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputTitle}>Name</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Task"
+          placeholderTextColor="#ddd"
+          onChangeText={(text) => setName(text)}
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputTitle}>Hour</Text>
+        <DateTimePicker
+          value={date}
+          mode={"time"}
+          is24Hour={true}
+          onChange={(event, selectedDate) => setDate(selectedDate)}
+          style={{ width: "80%" }}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputTitle}>Today</Text>
+        <Switch value={isToday} onValueChange={(value) => setIsToday(value)} />
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Done</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F7F8FA",
+    paddingHorizontal: 30,
+    marginTop: 50,
+  },
+  title: {
+    fontSize: 34,
+    fontWeight: "bold",
+    marginBottom: 35,
+    marginTop: 10,
+  },
+  inputTitle: {
+    fontSize: 20,
+    fontWeight: "600",
+    lineHeight: 24,
+  },
+  textInput: {
+    borderBottomColor: "#000030",
+    borderBottomWidth: 1,
+    width: "80%",
+  },
+  inputContainer: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    paddingBottom: 30,
+  },
+  button: {
+    marginTop: 30,
+    marginBottom: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "black",
+    height: 45,
+    borderRadius: 11,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
